b8d08a1de73cfa3fad2cf3c6e6c25f6f96c01aa8
DemetriSam <samartsew@gmail.com>
2022-09-29 02:09:15 +0300

    stringify

--- src/Functions.php
+++ src/Functions.php
@@ -76,6 +76,31 @@ function stringify($input, $replacer = ' ', $spacesCount = 1)
     return toString($input);
 }
 
+function stringifyT($value, string $replacer = ' ', int $spacesCount = 1): string
+{
+    $iter = function ($currentValue, $depth) use (&$iter, $replacer, $spacesCount) {
+        if (!is_array($currentValue)) {
+            return toString($currentValue);
+        }
+
+        $indentSize = $depth * $spacesCount;
+        $currentIndent = str_repeat($replacer, $indentSize);
+        $bracketIndent = str_repeat($replacer, $indentSize - $spacesCount);
+
+        $lines = array_map(
+            fn($key, $val) => "{$currentIndent}{$key}: {$iter($val, $depth + 1)}",
+            array_keys($currentValue),
+            $currentValue
+        );
+
+        $result = ['{', ...$lines, "{$bracketIndent}}"];
+
+        return implode("\n", $result);
+    };
+
+    return $iter($value, 1);
+}
+
 function printDiffInTerminal($diff)
 {
     $old = '-';
