24fe394a68ebeab2683d38f90b733d790e1f6be8
DemetriSam <samartsew@gmail.com>
2022-10-07 16:48:15 +0300

    printDiff

--- src/Functions.php
+++ src/Functions.php
@@ -8,8 +8,6 @@ function genDiff(string $first, string $second, string $formatName = 'stylish')
     $second = parseFile($second);
 
     $diff = prepareDiff($first, $second, $formatName);
-    
-    echo $diff;
 
     return $diff;
 }
@@ -19,4 +17,9 @@ function prepareDiff(array $first, array $second, $formatName)
     $keysCommonTree = buildKeysCommonTree($first, $second);
     $records = record($keysCommonTree, $first, $second);
     return render($records, $formatName);
+}
+
+function printDiff(string $first, string $second, string $formatName = 'stylish')
+{
+    echo genDiff($first, $second, $formatName);
 }
