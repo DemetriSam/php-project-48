0be766af9bd53af7185d69df55c3073e3183354a
DemetriSam <samartsew@gmail.com>
2022-10-22 13:59:58 +0300

    collectLines

--- src/Formatters/PlainFormatter.php
+++ src/Formatters/PlainFormatter.php
@@ -6,97 +6,67 @@ use Differ\Differ;
 
 function render(array $tree)
 {
-    $basket = [];
-    $lines = reduce(putStringInBasket(), $tree, $basket);
+    $lines = collectLines($tree);
     return implode("\n", $lines);
 }
 
-/**
- * Функция reduce аналог array_reduce, только для дерева
- */
-function reduce(callable $putStringInBasket, array $tree, array $init, array $path = [])
+function collectLines(array $node, array $lines = [], array $path = [])
 {
-    $type = Differ\getType($tree);
-    $key = Differ\getKey($tree);
+    $type = Differ\getType($node);
+    $key = Differ\getKey($node);
+    $currentPath = implode('.', putKeyToPath($path, $key));
 
     switch ($type) {
         case 'root':
         case 'nested':
-            $recursiveAcc = $putStringInBasket($init, $tree, $path);
-            $children = Differ\getChildren($tree);
-
+            $children = Differ\getChildren($node);
             return array_reduce(
                 $children,
-                fn($acc, $child) => reduce(
-                    $putStringInBasket,
+                fn($lines, $child) => collectLines(
                     $child,
-                    $acc,
-                    array_filter(
-                        array_merge($path, [$key]),
-                        fn($item) => $item !== null
-                    )
+                    $lines,
+                    putKeyToPath($path, $key),
                 ),
-                $recursiveAcc
+                $lines
             );
 
         case 'changed':
+            [$value1, $value2] = Differ\getValue($node);
+            $renderedValue1 = stringify($value1);
+            $renderedValue2 = stringify($value2);
+            return array_merge(
+                $lines,
+                ["Property '{$currentPath}' was updated. From {$renderedValue1} to {$renderedValue2}"]
+            );
+
         case 'deleted':
+            return array_merge(
+                $lines,
+                ["Property '{$currentPath}' was removed"]
+            );
+
         case 'added':
+            $value = Differ\getValue($node);
+            $renderedValue = stringify($value);
+            return array_merge(
+                $lines,
+                ["Property '{$currentPath}' was added with value: {$renderedValue}"]
+            );
+
         case 'unchanged':
-            return $putStringInBasket($init, $tree, $path);
+            return $lines;
+
         default:
             throw new \Exception("Unknown or not existed state");
     }
 }
 
-/**
- * Возвращает callback для reduce
- */
-function putStringInBasket()
+function putKeyToPath($path, $key)
 {
-    return function ($init, $node, $path) {
-
-        $type = Differ\getType($node);
-        $key = Differ\getKey($node);
-        $currentPath = implode('.', array_merge($path, [$key]));
-
-        switch ($type) {//кладем в корзинку к reduce строку, сформированную на основе данных и состояния ноды
-            case 'changed':
-                [$value1, $value2] = Differ\getValue($node);
-
-                $renderedValue1 = stringify($value1);
-                $renderedValue2 = stringify($value2);
-
-                return array_merge(
-                    $init,
-                    ["Property '{$currentPath}' was updated. From {$renderedValue1} to {$renderedValue2}"]
-                );
-
-            case 'deleted':
-                return array_merge(
-                    $init,
-                    ["Property '{$currentPath}' was removed"]
-                );
-
-            case 'added':
-                $value = Differ\getValue($node);
-                $renderedValue = stringify($value);
-
-                return array_merge(
-                    $init,
-                    ["Property '{$currentPath}' was added with value: {$renderedValue}"]
-                );
-
-            case 'root':
-            case 'nested':
-            case 'unchanged':
-                return $init;
-                //для тех состояний, которые не должны попадать в вывод, не кладем в корзинку ничего
-
-            default:
-                throw new \Exception("Unknown or not existed state");
-        }
-    };
+    return array_filter(
+        array_merge($path, [$key]),
+        fn($item) => $item !== null
+    );
 }
 
 function stringify($value)
